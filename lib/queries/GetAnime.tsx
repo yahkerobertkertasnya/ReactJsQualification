bea296fcceca5844d737b564034247deafc58d5f
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2023-04-30 13:36:34 +0700

    test

--- lib/queries/GetAnime.tsx
+++ lib/queries/GetAnime.tsx
@@ -26,40 +26,40 @@ query getAllAnime {
       }
     }
   }
+  Page3: Page(page:3, perPage:50){
+  media(type:ANIME, sort:SCORE_DESC){
+    id
+    coverImage{
+        large
+    }
+    title{
+      english
+    }
+  }
+}
+Page4: Page(page:4, perPage:50){
+   media(type:ANIME, sort:SCORE_DESC){
+     id
+     coverImage{
+         large
+     }
+     title{
+       english
+     }
+   }
+ }
+Page5: Page(page:5, perPage:50){
+  media(type:ANIME, sort:SCORE_DESC){
+    id
+    coverImage{
+        large
+    }
+    title{
+      english
+    }
+  }
+}
 }`;
 
 
 
-// Page3: Page(page:3, perPage:50){
-//   media(type:ANIME, sort:SCORE_DESC){
-//     id
-//     coverImage{
-//         large
-//     }
-//     title{
-//       english
-//     }
-//   }
-// }
-// Page4: Page(page:4, perPage:50){
-//    media(type:ANIME, sort:SCORE_DESC){
-//      id
-//      coverImage{
-//          large
-//      }
-//      title{
-//        english
-//      }
-//    }
-//  }
-// Page5: Page(page:5, perPage:50){
-//   media(type:ANIME, sort:SCORE_DESC){
-//     id
-//     coverImage{
-//         large
-//     }
-//     title{
-//       english
-//     }
-//   }
-// }
