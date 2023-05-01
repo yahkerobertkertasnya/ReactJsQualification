7154d1ac3950c5bbe0394366c8863ee49bdb4bed
yahkerobertkertasnya <70179934+yahkerobertkertasnya@users.noreply.github.com>
2023-05-01 22:00:59 +0700

    5/1/2023 10:00

--- components/Favorite/FavoriteButton.tsx
+++ components/Favorite/FavoriteButton.tsx
@@ -48,8 +48,8 @@ function FavoriteButton({id} : {id : number}){
 
 
     return(
-        <button className="btn p-0 mb-4 shadow-none btn-link" onClick={() => { setFavorite(!isFavorite) }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="4vw" height="4vw" fill={ isFavorite ? "red" : "white" } className="bi bi-heart-fill" viewBox="0 0 16 16">
+        <button className="btn p-0 shadow-none btn-link" style={{ marginBottom: "1em"}} onClick={() => { setFavorite(!isFavorite) }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="4vw" height="10%" fill={ isFavorite ? "red" : "white" } className="bi bi-heart-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
             </svg>
         </button>
